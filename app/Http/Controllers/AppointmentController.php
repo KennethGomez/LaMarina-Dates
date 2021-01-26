d13724f082c9d658d6a2e41c7a89b5c0a43562c0
KennethGómez <kennethgomezfdz@gmail.com>
2021-01-26 13:42:00 +0100

    Add custom exception handling with base class

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\AppException;
+use App\Exceptions\Date\DateNotAvailableException;
 use App\Models\Appointment;
 use App\Repositories\Appointment\AppointmentRepositoryInterface;
 use App\Repositories\Date\DateRepositoryInterface;
-use Carbon\Carbon;
 use Carbon\Exceptions\Exception;
 use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class AppointmentController extends Controller
@@ -41,7 +43,7 @@ class AppointmentController extends Controller
      *
      * @return Renderable
      */
-    public function index()
+    public function index(): Renderable
     {
         $appointments = $this->repository->paginated(20, 'date');
 
@@ -52,7 +54,9 @@ class AppointmentController extends Controller
      * Stores an appointment to database
      *
      * @param Request $request
-     * @return \Illuminate\Http\RedirectResponse|object
+     * @return RedirectResponse|object
+     *
+     * @throws AppException|DateNotAvailableException
      */
     public function store(Request $request)
     {
@@ -78,10 +82,7 @@ class AppointmentController extends Controller
                 ->setStatusCode(201)
                 ->with(['success' => __('La cita se ha guardado exitosamente')]);
         } catch (Exception) {
-            return redirect()
-                ->route('index')
-                ->setStatusCode(400)
-                ->withErrors(['error' => __('Error indefinido, contacte con un administrador')]);
+            throw new AppException();
         }
     }
 
@@ -89,7 +90,9 @@ class AppointmentController extends Controller
      * Destroys an appointment of the database
      *
      * @param Appointment $appointment
-     * @return \Illuminate\Http\RedirectResponse|object
+     * @return RedirectResponse|object
+     *
+     * @throws AppException
      */
     public function destroy(Appointment $appointment)
     {
@@ -101,10 +104,7 @@ class AppointmentController extends Controller
                 ->setStatusCode(201)
                 ->with(['success' => __('La cita se ha eliminado exitosamente')]);
         } catch (\Exception) {
-            return redirect()
-                ->route('appointments.index')
-                ->setStatusCode(400)
-                ->withErrors(['error' => __('Error indefinido, contacte con un administrador')]);
+            throw new AppException();
         }
     }
 }
